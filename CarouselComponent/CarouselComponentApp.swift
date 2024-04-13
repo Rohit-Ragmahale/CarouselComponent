1e7b92b9ba9c344b835dadbfb31d09abfa96e4b0
Rohit Shankar Ragmahale <rohitragmahale.mobile@gmail.com>
2024-04-13 13:24:08 +0530

    Initial Commit

---
+++ CarouselComponent/CarouselComponentApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarouselComponentApp.swift
+//  CarouselComponent
+//
+//  Created by Rohit Ragmahale on 13/04/24.
+//
+
+import SwiftUI
+
+@main
+struct CarouselComponentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
