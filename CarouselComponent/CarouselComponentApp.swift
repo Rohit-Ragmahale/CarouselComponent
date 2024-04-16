3c908d6722c234ef736e7391d967dc4b3ab9c467
Rohit Shankar Ragmahale <rohitragmahale.mobile@gmail.com>
2024-04-16 07:48:37 +0530

    helper links

--- CarouselComponent/CarouselComponentApp.swift
+++ CarouselComponent/CarouselComponentApp.swift
@@ -5,6 +5,13 @@
 //  Created by Rohit Ragmahale on 13/04/24.
 //
 
+
+// https://www.youtube.com/watch?v=DgTPWYM5Hm4
+// https://stackoverflow.com/questions/77169478/custom-image-slider-view-in-swiftui
+// https://www.appcoda.com/scrollview-paging/#google_vignette
+// https://www.youtube.com/watch?v=3zBSgXoSugU
+
+
 import SwiftUI
 let colors: [Color] = [.red, .orange, .blue, .teal, .mint, .green, .gray, .indigo, .black]
 
